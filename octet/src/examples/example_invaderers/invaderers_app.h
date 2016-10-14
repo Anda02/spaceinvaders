d6f18520d8c878d4d34758ecdaa2d7ae8ec7867a
Andrew Sherman <anda02@gmail.com>
2016-10-14 13:26:09 +0100

    Player can hold down more than one button at a time to let the ship move across multiple axis at once.

--- octet/src/examples/example_invaderers/invaderers_app.h
+++ octet/src/examples/example_invaderers/invaderers_app.h
@@ -246,19 +246,19 @@ namespace octet {
           sprites[ship_sprite].translate(+ship_speed, 0);
         }
       } 
-	  else if (is_key_down(key_right)) {
+	  if (is_key_down(key_right)) {
         sprites[ship_sprite].translate(+ship_speed, 0);
         if (sprites[ship_sprite].collides_with(sprites[first_border_sprite+3])) {
           sprites[ship_sprite].translate(-ship_speed, 0);
         }
       }
-	  else if (is_key_down(key_up)) {
+	  if (is_key_down(key_up)) {
 		  sprites[ship_sprite].translate(0, +ship_speed);
 		  if (sprites[ship_sprite].collides_with(sprites[first_border_sprite + 3])) {
 			  sprites[ship_sprite].translate(0, -ship_speed);
 		  }
 	  }
-	  else if (is_key_down(key_down)) {
+	  if (is_key_down(key_down)) {
 		  sprites[ship_sprite].translate(0, -ship_speed);
 		  if (sprites[ship_sprite].collides_with(sprites[first_border_sprite + 3])) {
 			  sprites[ship_sprite].translate(0, +ship_speed);
